909eef8d41bc99519fce769026778973738c06b1
Christian Puchaicela <christianmls@hotmail.com>
2022-02-09 14:06:53 -0500

    Merge

--- app.js
+++ app.js
@@ -6,16 +6,10 @@ const Validation = require('./api/config/validation');
 
 const PORT = process.env.PORT || 8080;
 
-<<<<<<< HEAD
-app.use(express.json({limit: '50mb'}));
-
-=======
 const result = dotenv.config()
-
 console.log('Resultado:', result);
 
-app.use(express.json());
->>>>>>> 92c4a970576faa5f5edda5c8962cc8775d79ee78
+app.use(express.json({limit: '50mb'}));
 app.use('/pdf', PDFRoutes);
 
 Validation.validateRequirements();
